537e60344c2613ceeb1b4b0f90ff035ab9eac1b2
AdithyaBR <adithya7760@gmail.com>
2025-07-19 13:34:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+# ====================== Terraform Provider Configuration ======================
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# ====================== VPC Configuration with Subnets ======================
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "Patient-Appointment-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
