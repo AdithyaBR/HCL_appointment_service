67b0a5095c9dc0d5ac5ebf771c01f2c2db51a5c7
AdithyaBR <adithya7760@gmail.com>
2025-07-19 17:20:01 +0530

    Update ecr.tf

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,12 @@
 resource "aws_ecr_repository" "appointment_service" {
   name = "appointment-service"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name        = "appointment-service-ecr"
+    Environment = "dev"
+  }
 }
