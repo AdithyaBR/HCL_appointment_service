27665eaed8acd9e874d3c00a5018c7642b1cb83f
AdithyaBR <adithya7760@gmail.com>
2025-07-19 16:13:35 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,41 @@
-variable "fargate_cpu" {
-  description = "The number of CPU units used by the task"
+variable "ecs_cluster_name" {
+  description = "ECS Cluster Name"
   type        = string
-  default     = "512" # 0.5 vCPU
+  default     = "healthcare-cluster"
 }
 
-variable "fargate_memory" {
-  description = "The amount of memory (in MiB) used by the task"
+variable "ecs_task_execution_role_name" {
+  description = "IAM role for ECS task execution"
   type        = string
-  default     = "1024" # 1 GB
+  default     = "ecs-task-execution-role2"
+}
+
+variable "task_family_name" {
+  description = "Task Definition Family Name"
+  type        = string
+  default     = "appointment-service"
+}
+
+variable "container_name" {
+  description = "Name of the ECS container"
+  type        = string
+  default     = "appointment-service"
+}
+
+variable "container_port" {
+  description = "Port used by the ECS container"
+  type        = number
+  default     = 3001
+}
+
+variable "log_group_name" {
+  description = "Log group for ECS containers"
+  type        = string
+  default     = "/ecs/appointment-service"
+}
+
+variable "desired_count" {
+  description = "Number of ECS tasks to run"
+  type        = number
+  default     = 1
 }
