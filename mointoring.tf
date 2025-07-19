ad7d3fa71401c54083597b8c8c8d6b0f84a638f0
AdithyaBR <adithya7760@gmail.com>
2025-07-19 13:32:01 +0530

    Create mointoring.tf

---
+++ mointoring.tf
@@ -0,0 +1,22 @@
+resource "aws_cloudwatch_log_group" "appointment_service" {
+  name              = "/ecs/appointment-service"
+  retention_in_days = 7
+}
+
+# CloudWatch Alarms (Example for CPU)
+resource "aws_cloudwatch_metric_alarm" "high_cpu_patient" {
+  alarm_name          = "patient-service-high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 70
+
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.healthcare_cluster.name
+    
+  }
+}
