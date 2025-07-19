675cde9e3b4ea88e870a511cc32f2730da87d92e
AdithyaBR <adithya7760@gmail.com>
2025-07-19 17:20:22 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "patient-appointment-app-tfstate-bucket" 
+    bucket         = "patient-appointment-app-tfstate-bucket"  # Must exist
     key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock" 
+    region         = "us-west-1"
     encrypt        = true
+    dynamodb_table = "terraform-lock-table"  # Optional but recommended
   }
 }
